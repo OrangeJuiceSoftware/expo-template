4b9df0fe6d1a1f9e542d5606ee9d82ac3c47048a
Eric Leong <eleong95@gmail.com>
2019-08-03 12:44:05 -0700

    email and password sign up

--- src/screenComponents/authentication/SignInScreen.js
+++ src/screenComponents/authentication/SignInScreen.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { Container, Header, Content, Form, Item, Input, Button, Icon } from 'native-base';
 
 import firebase from '../../../FirebaseInit';
 const firebaseAuth = firebase.auth();
 
+const useInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+
+  return [
+    value,
+    {
+      value,
+      onChangeText: text => {
+        setValue(text);
+      }
+    },
+    () => setValue(""),
+    setValue
+  ];
+};
+
 export default function SignInScreen() {
+  const [email, bindEmail, resetEmail] = useInput();
+  const [password, bindPassword, resetPassword] = useInput();
+
+  const [emailError, setEmailError] = useState()
+
   const signInAsGuest = async () => {
     try {
       await firebaseAuth.signInAnonymously()
@@ -13,16 +36,34 @@ export default function SignInScreen() {
     }
   }
 
+  const signUpWithEmail = async (email, password) => {
+    try {
+      await firebase.auth().createUserWithEmailAndPassword(email, password)
+    } catch (e) {
+      setEmailError(e)
+    }
+  }
+
   return (
-    <Text onPress={signInAsGuest}>Continue as guest</Text>
+    <Container>
+      <Content>
+        <Form>
+          <Item>
+            <Input {...bindEmail} placeholder={'email'}/>
+            {emailError && <Text>{emailError.code}</Text>}
+          </Item>
+
+          <Item last>
+            <Input {...bindPassword} placeholder={'password'}/>
+          </Item>
+        </Form>
+
+        <Button onPress={() => signUpWithEmail(email, password)}>
+          <Text>Sign Up</Text>
+        </Button>
+
+        <Text onPress={signInAsGuest}>Continue as guest</Text>
+      </Content>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
